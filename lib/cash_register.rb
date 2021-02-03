03ed75135b2dc4e3d44287fdd1e135a4cd4f07c6
Da-Mell <Damellfaines8@gmail.com>
2021-02-02 20:48:13 -0500

    commit tree 9bf65ca81ac11b31820d27c120a9779a27a483a3

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
 
+    class CashRegister 
+    attr_accessor :total,:discount,:cart
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = 0
+        @cart = []
+        @employee_discount=0
+    end
+
+
+    def add_item(item, price, quantity = 1)
+    item_info = {}
+    item_info[:name] = item
+    item_info[:price] = price
+    item_info[:quantity] = quantity
+
+    @cart << item_info
+
+    @total += price * quantity
+    end
+
+    def applies_discount(total, discount)
+
+    end
+    end
+
